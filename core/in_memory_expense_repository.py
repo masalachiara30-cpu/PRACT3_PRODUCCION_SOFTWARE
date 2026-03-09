8b09473bafe135fd0f0aadb3453cadf84175452d
Chiara <masalachiara30@gmail.com>
2026-03-09 18:24:00 +0000

    fix formatting

--- core/in_memory_expense_repository.py
+++ core/in_memory_expense_repository.py
@@ -14,9 +14,9 @@ class InMemoryExpenseRepository(ExpenseRepository):
         self._expenses.append(expense)
 
     def remove(self, expense_id: int) -> None:
-     self._expenses = [
-        expense for expense in self._expenses if expense.id != expense_id
-     ]
+        self._expenses = [
+            expense for expense in self._expenses if expense.id != expense_id
+        ]
 
     def get_by_id(self, expense_id: int) -> Expense | None:
         return next(
